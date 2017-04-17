1226344d329179717c9d0e977d1a3f9e23fa5ede
Alexander Kubkin <ioc0kernel@gmail.com>
2017-04-17 10:54:42 +0300

    Changes Made

--- src/extension.ts
+++ src/extension.ts
@@ -6,49 +6,97 @@ import * as vscode from 'vscode';
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+console.log('Congratulations, your extension "test" is now active!');
+let editor = vscode.window.activeTextEditor;
+const smallNumberDecorationType = vscode.window.createTextEditorDecorationType({
+		borderWidth: '1px',
+		borderStyle: 'solid',
+		overviewRulerColor: 'blue',
+		overviewRulerLane: vscode.OverviewRulerLane.Right,
+		light: {
+			// this color will be used in light color themes
+			borderColor: 'darkblue'
+		},
+		dark: {
+			// this color will be used in dark color themes
+			borderColor: 'lightblue'
+		}
+	});    
+    const sampleDecorationType = vscode.window.createTextEditorDecorationType({
+		cursor: 'crosshair',
+		backgroundColor: 'rgba(255,155,0,0.3)'
+	});
+    const sampleDecoration: vscode.DecorationOptions[] = [];
+    let activeEditor = vscode.window.activeTextEditor;
+    function updateDecorations(startP,endP){
+        if(!activeEditor){
+            return;
+        }
+        let startPos = activeEditor.document.positionAt(startP);
+        let endPos = activeEditor.document.positionAt(endP);
+        let decoration = {range: new vscode.Range(startPos, endPos), 
+		 		hoverMessage: 'This Part is intersting' + '**'}
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "test" is now active!');
+        sampleDecoration.push(decoration);
+        activeEditor.setDecorations(sampleDecorationType,sampleDecoration);
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    let disposable = vscode.commands.registerCommand('extension.someCommand', () => {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        var net = require('net');
-		var server = net.createServer(function(socket, request, response) {
-				socket.write('Echo server\r\n');
-				socket.pipe(socket);
-                var listened = socket.listen();
-                socket.write(listened);       
-                
-                  
-            });
-            vscode.window.showInformationMessage('Server started and ready')
-		server.listen(1337, '127.0.0.1');
-            
-    });
-    let disposable2 = vscode.commands.registerCommand('extension.playerCommand',() =>{
-        var net = require('net');
-        var client = new net.Socket();
-        client.connect(1337, '127.0.0.1', function() {
-        console.log('Connected');
         
-        client.write('hello');
-        });
-        client.on('data', function(data) {
-        console.log('Received: ' + data);
-        vscode.window.showInformationMessage(data);
-       // client.destroy(); // kill client after server's response
-       });
-        //client.on('close', function() {
-        //console.log('Connection closed');
-        //});
-        vscode.window.showInformationMessage('Client Started');
+    }
+    let disposable = vscode.commands.registerCommand('extension.someCommand', () => {
+            var server = require('net');
+                 
+            
+            server.createServer(function(socket){
+                socket.write('Welcome \n');
+                socket.on('data', function (chunk) {
+                    var readable = chunk.toString();
+                    if (readable == 1){
+                        updateDecorations(7,13);
+                        updateDecorations(31,37);
+                        updateDecorations(55,61);
+                        socket.write('decoration updated');
+
+                    }
+                    if (readable == 2){
+                         updateDecorations(35,50);
+                        socket.write('decoration updated');
+                    }
+                    if (readable == 3){
+                        let someText =  editor.document.getText();
+                        console.log(someText);
+                        socket.write(someText);
+                        console.log('Text Sended');
+                    }
+                    console.log(readable);
+                    });
+               
+            let disposable2 = vscode.commands.registerCommand('extension.playerCommand',() =>{
+        
+            vscode.window.showInformationMessage('Client Started');
     } );
+            
+        }).listen(5000);
+            console.log('server started port 5000')
+                    
+          console.log('Server Started');  
+    });
+    //trying to make multicursor
+    let multiCursorButton = vscode.commands.registerCommand('extension.mCursorCommand',()=>{
+        var o = {};
+        //o.test = 'test';
+        //var cursor = vscode.window.showQuickPick()
+    });
+    // let sendText = vscode.commands.registerCommand('extension.sendTextCommand',() =>{
+    //             let editor = vscode.window.activeTextEditor;
+    //             let someText =  editor.document.getText();
+    //             console.log(someText);
+    //             //socket.write('someText');
+    //             vscode.window.showInformationMessage('Text Sended');
+    //         });
+    
+    
+       
+    
 
     context.subscriptions.push(disposable);
 }
